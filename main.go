2a0ccd15dbeb74907afd700bba309642fb82cf50
snailatspace <snailatspace@gmail.com>
2025-01-21 02:14:37 +0300

    Polishing changes and some other shit idk

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"syscall"
 	"time"
+	"math"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -89,10 +90,14 @@ func guildCreate(s *discordgo.Session, m *discordgo.GuildCreate){
 	fmt.Println("Joined server "+m.Guild.Name)
 	for i := 0; i<len(channels); i++ {
 		perms, _ := s.State.UserChannelPermissions("1330935741018276022", channels[i].ID)
-		if channels[i].Type == 0 && (perms & 2048 == 2048) {
+		if channels[i].Type == 0 && (perms & 2048 == 2048) && time.Now().Unix()-m.JoinedAt.Unix() < 30{
 			s.ChannelMessageSend(channels[i].ID, "The Server will be well-cared for.\n...After all, the onus always fell on me to give roles that you abandoned.")
 			return
 		}
+	}
+
+	if m.Guild.ID == "1250579779837493278" {
+		s.UpdateCustomStatus("Allow me to regale thee... that, in this... adventure of mine... Verily, I was blessed with a family of " + strconv.Itoa(m.Guild.MemberCount-1) + ".")
 	}
 }
 
@@ -100,12 +105,19 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-
+	var refid string
+	if m.ReferencedMessage != nil {
+		refid = m.ReferencedMessage.Author.ID
+	}
+	re := regexp.MustCompile("fuck|shit|ass|idiot|dumb|stupid|clanker")
 	if strings.HasPrefix(m.Content, ".roll ") {
 		roll(s, m)
-	}
-	if m.Content == ".bod" {
+	} else if m.Content == ".bod" {
 		bod(s, m)
+	} else if re.MatchString(m.Content) && (refid == s.State.User.ID || strings.Contains(strings.ToLower(m.Content), "sancho")){
+		fut(s,m)
+	} else if strings.Contains(m.Content, "conceived") && m.Author.ID == "530516460712361986"{
+		conceived(s,m)
 	}
 }
 
@@ -158,8 +170,8 @@ func roll(s *discordgo.Session, m *discordgo.MessageCreate){
 		}
 
 		sieveSimple := regexp.MustCompile("[0-9]+")
-		sieve := regexp.MustCompile("[0-9]+[+*-][0-9]+")
-		var modRune rune
+		sieve := regexp.MustCompile(`[0-9]+[\+\*\-\^_][0-9]+`)
+		var modRune int
 		if !sieve.MatchString(rest) {
 			if !sieveSimple.MatchString(rest){
 				iKnowWhatYouAre(s, c, m)
@@ -167,7 +179,7 @@ func roll(s *discordgo.Session, m *discordgo.MessageCreate){
 			}
 			max,_ = strconv.Atoi(rest)
 		} else {
-			modRune := strings.IndexAny(rest, "+*-")
+			modRune = strings.IndexAny(rest, "+*-^_")
 			max,_ = strconv.Atoi(rest[:modRune])
 			mod,_ = strconv.Atoi(rest[modRune+1:])
 		}
@@ -186,6 +198,7 @@ func roll(s *discordgo.Session, m *discordgo.MessageCreate){
 		}
 
 		if sieve.MatchString(rest){
+			// fmt.Println(string(rest[modRune]))
 			switch rest[modRune]{
 				case '+':
 					sum += mod
@@ -193,6 +206,10 @@ func roll(s *discordgo.Session, m *discordgo.MessageCreate){
 					sum *= mod
 				case '-':
 					sum -= mod
+				case '^':
+					sum = int(math.Pow(float64(sum), float64(mod)))
+				case '_':
+					sum = int(math.Pow(float64(mod), float64(sum)))
 			}
 		}
 
@@ -209,7 +226,19 @@ func bod(s *discordgo.Session, m *discordgo.MessageCreate){
 	if err != nil {
 		return
 	}
+	resp, err := http.Get("https://cdn.discordapp.com/attachments/927117412770394135/1331006346925178932/Shi1.png?ex=67900bc2&is=678eba42&hm=ee9cae73f48a5b64a9499f7b927313f56131115d103a7367832044058e42576b&")
+	if err != nil{
+		log.Fatal("FUCK")
+	}
+	defer resp.Body.Close()
+	yujin = resp.Body
 
+	resp, err = http.Get("https://tiphereth.zasz.su/static/assets/cards_thumb/Roland4Phase_Yujin.jpg")
+	if err != nil{
+		log.Fatal("FUCK")
+	}
+	defer resp.Body.Close()
+	yujinDead = resp.Body
 	
 	roll := rand.Intn(4)+1
 	if roll == 4{
@@ -227,10 +256,23 @@ func bod(s *discordgo.Session, m *discordgo.MessageCreate){
 			Content: strconv.Itoa(roll),
 			Files: []*discordgo.File{
 				{
-					Name: "yujinDead.png",
+					Name: "yujinDead.jpg",
 					Reader: yujinDead,
 				},
 			},
 		},)
 	}
+}
+
+func conceived(s *discordgo.Session, m *discordgo.MessageCreate){
+	s.ChannelMessageSendReply(m.ChannelID, "...What is it this time?", m.Reference())
+}
+
+func fut(s *discordgo.Session, m *discordgo.MessageCreate){
+	messages := []string{"...I won't even have to call Father for this.",
+	"...Hold your tongue; I will no longer tolerate any more \"ingenious ideas\".",
+	"You're nothing before a Second Kindred, let alone Father.",
+	"...Did you learn that from that Knight? How humorous.",
+	}
+	s.ChannelMessageSendReply(m.ChannelID, messages[rand.Intn(len(messages))], m.Reference())
 }
